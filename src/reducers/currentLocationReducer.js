1d23983bc9f84277eabd112e53140a2ee4693ade
Simon X <simonxiong11@gmail.com>
2021-07-16 20:05:45 -0400

    Create Redux state slice

--- src/reducers/currentLocationReducer.js
+++ src/reducers/currentLocationReducer.js
@@ -1,16 +1,45 @@
-const currentLocationDefaultState = {
-  currentLocation: ''
+// const currentLocationDefaultState = {
+//   currentLocation: ''
+// }
+
+// function currentLocationReducer (state = currentLocationDefaultState, action) {
+//   switch(action.type) {
+//     case 'CHANGE_LOCATION':
+//       return {
+//         ...state, ...action 
+//       }
+//     default:
+//       return state
+//   }
+// }
+
+// export default currentLocationReducer
+
+/* Redux Toolkit */
+
+//1. import createSlice API
+import { createSlice } from 'reduxjs/toolkit'
+
+//2. declare initial state 
+const initialState = {
+  value: '',
 }
 
-function currentLocationReducer (state = currentLocationDefaultState, action) {
-  switch(action.type) {
-    case 'CHANGE_LOCATION':
-      return {
-        ...state, ...action 
-      }
-    default:
-      return state
-  }
-}
+//3. declare slice container
+export const locationSlice = createSlice({
+  name: 'location',
+  initialState,
+  //4. declare reducer function
+  reducers: {
+    //5. declare action creator
+    changeLocation: (state, action ) => {
+      state.value = action.payload
+    },
+  },
+})
 
-export default currentLocationReducer
+//7. export action creator
+export const { changeLocation } = locationSlice.actions
+
+//8. export reducer function
+export default locationSlice.reducer
