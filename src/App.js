36f3f1adc9a78149c78f97db3b315dd827633f42
simonxcode <simonxiong11@gmail.com>
2021-12-23 19:29:30 -0500

    Set up and use TailwindCSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Page from './features/page';
 function App() {
   return (
     <div>
-    Clutch Weather App
+    <h1 className="text-4xl font-bold bg-blue-400">Clutch Weather App</h1>
       <Page />
     </div>
   );
