6ae769aaa769f1422222d3a33cfa02709c2cd0ec
simonxcode <simonxiong11@gmail.com>
2022-03-20 20:47:35 -0400

    Render daily high low temp

--- src/features/upcomingDaysDetails/UpcomingDaysDetails.jsx
+++ src/features/upcomingDaysDetails/UpcomingDaysDetails.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types'
 
 // const iconURLBase = 'https://www.metaweather.com/static/'
 
-const UpcomingDaysDetails = ({ /*iconURL,*/ weekday, /*lowTemp, highTemp*/ }) => (
+const UpcomingDaysDetails = ({ /*iconURL,*/ weekday, lowTemp, highTemp }) => (
   <li className="inline-block p-4 text-sky-900">
     {/*<img src={`${iconURLBase}img/weather/${iconURL}.svg`} alt='weather icon' className="w-12 mb-2" />*/}
     <p className="text-center font-semibold">{weekday}</p>
-    {/*<p className="text-center">{highTemp}° / {lowTemp}°</p>*/}
+    <p className="text-center">{highTemp} / {lowTemp}°F</p>
   </li>
 )
 
 UpcomingDaysDetails.propTypes = {
   // iconURL: PropTypes.string.isRequired,
   weekday: PropTypes.string.isRequired,
-  // highTemp: PropTypes.number.isRequired,
-  // lowTemp: PropTypes.number.isRequired
+  highTemp: PropTypes.string.isRequired,
+  lowTemp: PropTypes.string.isRequired
 }
 
 export default UpcomingDaysDetails
--- src/helpers/getUpcomingDays.js
+++ src/helpers/getUpcomingDays.js
@@ -2,12 +2,20 @@ import moment from 'moment'
 
 // const getWeekday = date => moment(date).format('dddd').substring(0, 3)
 
+const dailyHighTemp = day => {
+  return ((day.temp.max - 273.15) * 1.8 + 32.0).toFixed(0);
+};
+
+const dailyLowTemp = day => {
+  return ((day.temp.min - 273.15) * 1.8 + 32.0).toFixed(0);
+};
+
 const getUpcomingDays = data => 
   data.slice(1, 6).map(day => ({
     // iconURL: day.weather_state_abbr,
     weekday: moment.unix(day.dt).format('ddd'),
-    // highTemp: Math.round(day.max_temp),
-    // lowTemp: Math.round(day.min_temp)
+    highTemp: dailyHighTemp(day),
+    lowTemp: dailyLowTemp(day)
   }))
 
   export default getUpcomingDays
