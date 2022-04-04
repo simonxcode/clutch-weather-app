e1332dda5f991d42081452733f6ba92339913aa8
simonxcode <simonxiong11@gmail.com>
2022-04-03 20:45:48 -0400

    WIP - Proxy server with success response

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-//Routes
-app.use('/api', require('./routes'))
+//Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 Mins
+  max: 100
+})
+app.use(limiter)
+app.set('trust proxy', 1)
 
 //Enable cors
 app.use(cors())
+
+//Routes
+app.use('/api', require('./routes'))
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,32 @@
+const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const apicache = require('apicache')
 
 //Env vars
-const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL
-const REACT_APP_KEY_NAME = process.env.REACT_APP_KEY_NAME
-const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
+const BASE_URL = process.env.REACT_APP_BASE_URL
+const KEY_NAME = process.env.REACT_APP_KEY_NAME
+const API_KEY = process.env.REACT_APP_API_KEY
 
-router.get('/', async (req, res) => {
+//Init cache
+let cache = apicache.middleware
+
+router.get('/', cache('2 minutes'), async (req, res) => { 
   try {
     const params = new URLSearchParams({
-      [REACT_APP_KEY_NAME]: REACT_APP_API_KEY
+      ...url.parse(req.url, true).query,
+      [KEY_NAME]: API_KEY
+
     })
 
-    const apiRes = await needle('get', `${REACT_APP_BASE_URL}?${params}`)
+    const apiRes = await needle('get', `${BASE_URL}?${params}`)
     const data = apiRes.body
+
+    //Log request to public API 
+    if(process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${BASE_URL}?${params}`)
+    }
 
     res.status(200).json(data)
   } catch(error) {
