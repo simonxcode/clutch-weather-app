79a4e98b7411127cf904c28e64677d454a8d665c
simonxcode <simonxiong11@gmail.com>
2022-04-10 20:36:59 -0400

    Clean up express files

--- index.js
+++ index.js
@@ -2,31 +2,26 @@ const express = require('express')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
 require('dotenv').config()
-const errorHandler = require('./middleware/error')
+const errorHandler = require('./proxy-server/middleware/error')
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
-// Rate limiting
+app.use(cors())
+
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 Mins
-  max: 100,
+  windowMs: 30 * 1000, 
+  max: 5,
 })
 app.use(limiter)
 app.set('trust proxy', 1)
 
-// Enable cors
-app.use(cors())
-
-// Set static folder
 app.use(express.static('build'))
 
-// Routes
-app.use('/weather', require('./routes/weather'))
-app.use('/onecall', require('./routes/onecall'))
+app.use('/weather', require('./proxy-server/routes/weather'))
+app.use('/onecall', require('./proxy-server/routes/onecall'))
 
-// Error handler middleware
 app.use(errorHandler)
 
-app.listen(PORT, console.log(`Server is running on port ${ PORT}`))
+app.listen(PORT)
--- proxy-server/routes/onecall.js
+++ proxy-server/routes/onecall.js
@@ -1,32 +1,23 @@
-const url = require('url')
 const express = require('express')
 const router = express.Router()
+const url = require('url')
 const needle = require('needle')
 const apicache = require('apicache')
 
-//Env vars
 const BASE_URL = process.env.REACT_APP_BASE_URL
 const API_KEY = process.env.REACT_APP_API_KEY
 
-//Init cache
 let cache = apicache.middleware
 
-router.get('/', cache('2 minutes'), async (req, res, next) => {
+router.get('/', cache('1 minutes'), async (req, res, next) => {
   try {
     const params = new URLSearchParams({
       ...url.parse(req.url, true).query, 
-      // exclude: EXCLUDE,
       appid: API_KEY 
     })
 
     const apiRes = await needle('get', `${BASE_URL}/onecall?${params}&exclude={minutely,hourly,alerts}`)
     const data = apiRes.body
-
-    //Log request to public API 
-    if (process.env.NODE_ENV !== 'production') {
-      console.log(`REQUEST: ${BASE_URL}?${params}`)
-      console.log('response two', data)
-    }
 
     res.status(200).json(data)
   } catch (error) {
