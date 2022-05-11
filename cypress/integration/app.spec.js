5e3ed24088f935a25292a6562774f11b1793ce38
simonxcode <simonxiong11@gmail.com>
2022-05-11 13:27:30 -0400

    fix failing test cases

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,11 +1,4 @@
 describe('Clutch Weather App', () => {
-  beforeEach(() => {
-    cy.on('uncaught:exception', (err, runnable, promise) => {
-      if (promise) return false
-    cy.get('[data-cy=form-input]').type('atlanta');
-    cy.get('[data-cy=form-button]').click();
-    })
-  })
 
   it('should render ClutchWeatherApp content', () => {
     cy.visit('/')
@@ -24,32 +17,14 @@ describe('Clutch Weather App', () => {
     cy.get('[data-cy=form-button]').should('have.text', 'search')
   })
 
-  it('should allow a city to be entered and searched on Form', () => {
+  it('should allow a city to be searched and render application content', () => {
     cy.get('[data-cy=form-input]').type('atlanta');
     cy.get('[data-cy=form-button]').click();
-  })
-
-  it('should render Forecast content', () => {
     cy.get('[data-cy=forecast-container]').should('exist')
-  })
-
-  it('should render CurrentDay content', () => {
     cy.get('[data-cy=current-day]').should('exist')
-  })
-
-  it('should render CurrentDayDetails content', () => {
     cy.get('[data-cy=current-day-details]').should('exist')
-  })
-
-  it('should render CurrentDayDetailsItems content', () => {
     cy.get('[data-cy=current-day-details-items]').should('have.length', 7)
-  })
-
-  it('should render UpcomingDays content', () => {
     cy.get('[data-cy=upcoming-days]').should('exist')
-  })
-
-  it('should render UpcomingDaysDetails content', () => {
     cy.get('[data-cy=upcoming-days-details]').should('have.length', 5)
   })
 
